80435a913e31eba62237dffb7a7bf798ae690e92
sububrown <35603132+sububrown@users.noreply.github.com>
2020-01-29 23:19:42 +0530

    Added local variable 'z'.

--- Demo1.java
+++ Demo1.java
@@ -6,6 +6,7 @@ class Demo1{
    public static void main(String[] args){
       int x=0;
       int y=0;
+      int z=0;
       System.out.println("Hello World");
       System.out.println(x+y);
    }
