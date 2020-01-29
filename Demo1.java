105ce5d64e9b78b19690930b8dd7ce9e43ca36a6
sububrown <35603132+sububrown@users.noreply.github.com>
2020-01-29 23:25:20 +0530

    Added variable 'z'

--- Demo1.java
+++ Demo1.java
@@ -6,7 +6,8 @@ class Demo1{
    public static void main(String[] args){
       int x=10;
       int y=12;
+      int z=15;
       System.out.println("Hello World");
-      System.out.println(x+y);
+      System.out.println(x+y+z);
    }
 }
