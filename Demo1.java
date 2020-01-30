91e07a6e1deb067be802b2348299107bf94091dc
sububrown <35603132+sububrown@users.noreply.github.com>
2020-01-30 12:58:06 +0530

    Issue resolved by doing adding as per #6
    Local variables 'a' and 'b' are added by assigning values.

--- Demo1.java
+++ Demo1.java
@@ -7,9 +7,10 @@ class Demo1{
       int x=10;
       int y=12;
       int z=15;
-      int a=0;
-      int b=0;
+      int a=25;
+      int b=35;
       System.out.println("Hello World");
       System.out.println(x+y+z);
+      System.out.println(a+b);
    }
 }
